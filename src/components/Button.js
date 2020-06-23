25283e11bb6f6dfa308e86bf941e672882013276
kevin.garay <kevingcfcb88@gmail.com>
2020-06-22 23:20:39 -0600

    Second Context added

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
 
 class Button extends React.Component {
   render() {
     return (
-      <button className="ui button primary">
-        <LanguageContext.Consumer>
-          {value => (value === 'en' ? 'Submit' : 'Voorleggen')}
-        </LanguageContext.Consumer>
-      </button>
+      <ColorContext.Consumer>
+        {color => (
+          <button className={`ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {value => (value === 'en' ? 'Submit' : 'Voorleggen')}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
     );
   }
 }
